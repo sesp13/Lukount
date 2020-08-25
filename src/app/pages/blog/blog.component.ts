c679cc235ab5074d866eecde4f7a1c9ceacb5ad4
sesp13 <sespinosarte99@gmail.com>
2020-08-25 14:00:02 -0500

    feat: cambios en contenidos

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -14,14 +14,14 @@ export class BlogComponent implements OnInit {
   ngOnInit(): void {
     this.contentPosts = [
       {
-        title: "Título para entrada de blog",
+        title: "¿Has oído hablar de la planeación financiera? FP&A",
         imagePath: "../../../assets/lukount/HOME/contacta_asesor.png",
         content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium animi modi facilis ea repellat excepturi, quidem cupiditate est veritatis laborum ut repellendus fugit facere quis laboriosam rerum omnis corrupti voluptates.",
         authorImage: "https://cdn.vox-cdn.com/thumbor/Oi6jzvQzWetJGlkpwLqlP1L9p28=/0x0:5568x3712/1200x800/filters:focal(2858x720:3748x1610)/cdn.vox-cdn.com/uploads/chorus_image/image/62207705/922984782.jpg.0.jpg",
         authorName: "Miguel Giner"
       },
       {
-        title: "Título para entrada de blog 2",
+        title: "¿Como creo mi empresa en Colombia?",
         imagePath: "../../../assets/lukount/HOME/contacta_asesor.png",
         content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium animi modi facilis ea repellat excepturi, quidem cupiditate est veritatis laborum ut repellendus fugit facere quis laboriosam rerum omnis corrupti voluptates.",
         authorImage: "https://cdn.vox-cdn.com/thumbor/Oi6jzvQzWetJGlkpwLqlP1L9p28=/0x0:5568x3712/1200x800/filters:focal(2858x720:3748x1610)/cdn.vox-cdn.com/uploads/chorus_image/image/62207705/922984782.jpg.0.jpg",
--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -7,36 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FAQComponent implements OnInit {
 
-  public questions; 
-  
+  public questions;
+
   constructor() { }
 
   ngOnInit(): void {
     this.questions = [
       {
-        question: "Pregunta número 1 de mi página web ?",
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aut voluptates sint et obcaecati voluptate velit, dolor in aspernatur autem repellat omnis deserunt modi quae quaerat tempore mollitia. Exercitationem, soluta?"
+        question: "¿Como te llevamos a Bordo?",
+        answer: "Cambiar de equipo contable puede ser un paso asustador ya que afecta a operaciones estratégicas de tus negocios. ¡No te preocupes! A partir del momento que te contactas con nosotros, tendrás un asesor financiero dedicado a analizar y diagnosticar el estado financiero de tu empresa. Si decides irte con nosotros, definiremos previamente un plan detallado de empalme con el antiguo equipo y así evitar lo que llamamos un “Black out”. En las primeras semanas dedicaremos un equipo reforzado de contadores y especialistas tecnológico para permitir una integración sin turbulencias y eficiente."
       },
       {
-        question: "Pregunta número 2 de mi página web ?",
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aut voluptates sint et obcaecati voluptate velit, dolor in aspernatur autem repellat omnis deserunt modi quae quaerat tempore mollitia. Exercitationem, soluta?"
+        question: "¡Sin costos adicionales!",
+        answer: "El precio que te daremos será un precio fijo mensual determinado gracias al análisis de tu información financiera. No habrá costos adicionales una vez determinamos junto el alcance de los servicios que quieras contratar."
       },
       {
-        question: "Pregunta número 3 de mi página web ?",
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aut voluptates sint et obcaecati voluptate velit, dolor in aspernatur autem repellat omnis deserunt modi quae quaerat tempore mollitia. Exercitationem, soluta?"
+        question: "¿Lukount puede realizar la liquidación de nómina de mi empresa?",
+        answer: "Tenemos un equipo de experto dedicado a realizar liquidación de nómina, así que no te preocupes, no hacemos cargo de todo al respecto."
       },
       {
-        question: "Pregunta número 4 de mi página web ?",
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aut voluptates sint et obcaecati voluptate velit, dolor in aspernatur autem repellat omnis deserunt modi quae quaerat tempore mollitia. Exercitationem, soluta?"
-      },
-      {
-        question: "Pregunta número 5 de mi página web ?",
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aut voluptates sint et obcaecati voluptate velit, dolor in aspernatur autem repellat omnis deserunt modi quae quaerat tempore mollitia. Exercitationem, soluta?"
-      },
-      {
-        question: "Pregunta número 6 de mi página web ?",
-        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aut voluptates sint et obcaecati voluptate velit, dolor in aspernatur autem repellat omnis deserunt modi quae quaerat tempore mollitia. Exercitationem, soluta?"
-      },
+        question: "¿Como accedo a mi información contables e informes?",
+        answer: "Toda la información estará disponible en la nube a través de nuestra plataforma, la cual tendrás un acceso permanente con conexión internet. De ahí podrás descargar todos los informes y piezas contables que necesites: declaraciones, facturas, orden de compra, recibos de nómina, balances e informes financieros, etc... También, te aclaramos que se hace de manera periódica una back up de toda la información. Puedes acercarte a nuestra política de manejo y protección de datos. Y si te queda alguna duda, nuestro equipo tecnológico te podrá aclararlas."
+      }
     ];
   }
 
